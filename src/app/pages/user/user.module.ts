46fa8f837708d3b9da2501c72fe3a78e80d3e18b
ANDRIATSIFERANA <andriatsiferana.nomena@gmail.com>
2021-07-09 11:21:59 +0300

    create user component info

---
+++ src/app/pages/user/user.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserAccountInfoComponent } from './user-account-info/user-account-info.component';
+
+
+
+@NgModule({
+  declarations: [
+    UserAccountInfoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserModule { }
